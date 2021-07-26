0f9caec9807fb0e83c3f1d341bcbe5ca982dc526
JuanSebastianGB <3393@holbertonschool.com>
2021-07-26 06:07:32 +0000

    adding files to print integers and decimals, task 1

---
+++ auxiliar_functions.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ *_print_rev - Prints a string in reverse mode
+ *@s: string to print
+ *
+ *Return: nothing
+ */
+
+void _print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
+
+/**
+ * _abs - Return always a positive number
+ * @n: number
+ *
+ * Return: positive number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n *= -1;
+	return (n);
+}

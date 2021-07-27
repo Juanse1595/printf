769471202016c53dc0bf1b801b55896e21abc2d9
Juanse1595 <3309@holbertonschool.com>
2021-07-27 03:24:53 +0000

    Adding print_hex, print_bin, print_rot13 and print_rev functions

--- print_char_functions.c
+++ print_char_functions.c
@@ -9,6 +9,7 @@
 int print_char(va_list c)
 {
 	char x = va_arg(c, int);
+
 	_putchar(x);
 	return (1);
 }
@@ -23,9 +24,61 @@ int print_str(va_list s)
 {
 	char *word = va_arg(s, char *);
 	int i;
+
 	for (i = 0; word[i]; i++)
 	{
 		_putchar(word[i]);
+	}
+	return (i);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to string
+ * Return: length of string
+ */
+
+int print_rev(va_list s)
+{
+	char *str = va_arg(s, char *);
+	int i, j;
+
+	for (i = 0, j = 0; str[i]; i++, j++)
+	{}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(str[i]);
+		i--;
+	}
+	return (j);
+}
+
+/**
+ * print_rot13 - prints a string with rot13 format
+ * @s: pointer to string to be changed
+ * Return: length of string
+ */
+
+int print_rot13(va_list s)
+{
+	char *arr1 = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char *arr2 = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char *original = va_arg(s, char *);
+	int i, j;
+
+	for (i = 0; original[i]; i++)
+	{
+		for (j = 0; arr1[j]; j++)
+		{
+			if (original[i] == arr1[j])
+			{
+				_putchar(arr2[j]);
+				break;
+			}
+		}
+		if (j == 52)
+			_putchar(original[i]);
 	}
 	return (i);
 }
--- print_number_functions.c
+++ print_number_functions.c
@@ -66,3 +66,91 @@ int print_decimal(va_list d)
 	_print_rev(store);
 	return (counter + 1);
 }
+
+/**
+ * print_bin - prints binary
+ * @n: Number to be changed
+ * Return: count of chars printed
+ */
+
+int print_bin(va_list n)
+{
+	int i, j, num;
+	char result[32], *resultinv;
+
+	i = 0;
+	num = va_arg(n, int);
+	while (num > 0)
+	{
+		if (num % 2 == 0)
+			result[i] = '0';
+		else
+			result[i] = '1';
+		num = num / 2;
+		i++;
+	}
+	result[i] = '\0';
+	resultinv = malloc(i + 1);
+	i--;
+	for (j = 0; i >= 0; i--, j++)
+	{
+		resultinv[j] = result[i];
+	}
+	resultinv[j] = '\0';
+	for (i = 0; resultinv[i] ; i++)
+	{
+		_putchar(resultinv[i]);
+	}
+	free(resultinv);
+	return (i);
+}
+
+/**
+ * print_hex - prints a num in hexadecimal
+ * @num: number to be printed
+ * Return: count of char printed
+ */
+
+int print_hex(va_list num)
+{int n = va_arg(num, int);
+	int aux = n, i = 0, j = 0, k, a = 1, aux2 = 0, count = 0;
+	int arr1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+10, 11, 12, 13, 14, 15, -1};
+	char arr2[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+'A', 'B', 'C', 'D', 'E', 'F'};
+
+	while (aux > 15)
+	{
+		aux = aux / 16;
+		i++;
+	}
+	while (i > 0)
+	{
+		while (j < i)
+		{
+			a = a * 16;
+			j++;
+		}
+		aux2 = n / a;
+		for (k = 0; arr1[k] >= 0; k++)
+		{
+			if (arr1[k] == aux2)
+			{
+				_putchar(arr2[k]);
+				count++;
+				break;
+			}
+		}
+		i--, n = n % a, j = 0, a = 1;
+	}
+	for (k = 0; arr1[k] >= 0; k++)
+	{
+		if (arr1[k] == n)
+		{
+			_putchar(arr2[k]);
+			count++;
+			break;
+		}
+	}
+	return (count);
+}
